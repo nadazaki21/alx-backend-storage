c599d2ef23cabd816b32b8121b8c1e1a6fb4e065
nadazkai21 <nadazaki019@gmail.com>
2024-08-13 23:46:28 +0300

    11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,6 @@
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
-    mongo_collection.find({"topics": topic})
+    x = mongo_collection.find({"topics": topic})
+    # print(mongo_collection)
+    return x
