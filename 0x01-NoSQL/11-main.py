b30120682fdc3ebfc55f1658174b70b3e257c384
nadazkai21 <nadazaki019@gmail.com>
2024-08-13 23:36:24 +0300

    11

--- 0x01-NoSQL/11-main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" Task 10 """
-
-
-def schools_by_topic(mongo_collection, topic):
-    """function that returns the list of school having a specific topic"""
-    mongo_collection.find({"topics": topic})
