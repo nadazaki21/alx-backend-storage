75a3aacb23ae05410d89e83a5ae04491670bdfe9
nadazkai21 <nadazaki019@gmail.com>
2024-08-14 01:41:56 +0300

    101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Task 14 """
+
+
+def top_students(mongo_collection):
+    """function that returns all students sorted by average score"""
+    for i in mongo_collection.find():
+        print(f"[{i['_id']}] {i['name']} - [{i['topics']}]")
+        sum_score = 0
+        for j in range(len(i["topics"])):
+            sum_score += i["topics"][j]["score"]
+        averageScore = sum_score / len(i["topics"])
+        # i.update_one({"_id" : i['_id']},{$set: {"averageScore": averageScore}})
+        mongo_collection.update_one(
+            {"_id": i["_id"]}, {"$set": {"averageScore": averageScore}}
+        )
+    return mongo_collection.find().sort("averageScore", -1)
