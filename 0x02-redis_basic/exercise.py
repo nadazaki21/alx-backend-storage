7b015488b2093b6792b940ef09b900189e1a49ef
nadazkai21 <nadazaki019@gmail.com>
2024-08-15 00:56:14 +0300

    trying to know what ismissing in 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -45,6 +45,17 @@ class Cache:
         """Get string from redis"""
         return data.decode('utf-8')
 
-    def get_int(self, data: Union[str, bytes, int, float]) -> int:
-        """Get int from redis"""
-        return data.decode('utf-8')
+    # def get_int(self, data: Union[str, bytes, int, float]) -> int:
+    #     """Get int from redis"""
+    #     return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''
+            Get an int from the cache.
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
