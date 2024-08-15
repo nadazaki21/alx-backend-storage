6f095d31b0afd93d84a6035266719f670ca75cf0
nadazkai21 <nadazaki019@gmail.com>
2024-08-15 07:03:55 +0300

    100

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,15 +10,15 @@ redis = redis.Redis()
 
 
 def wrap_requests(fn: Callable) -> Callable:
-    """Decorator wrapper"""
+    """ Decorator wrapper """
 
     @wraps(fn)
     def wrapper(url):
-        """Wrapper for decorator guy"""
+        """ Wrapper for decorator guy """
         redis.incr(f"count:{url}")
         cached_response = redis.get(f"cached:{url}")
         if cached_response:
-            return cached_response.decode("utf-8")
+            return cached_response.decode('utf-8')
         result = fn(url)
         redis.setex(f"cached:{url}", 10, result)
         return result
@@ -28,6 +28,7 @@ def wrap_requests(fn: Callable) -> Callable:
 
 @wrap_requests
 def get_page(url: str) -> str:
-    """get page self descriptive"""
+    """get page self descriptive
+    """
     response = requests.get(url)
     return response.text
