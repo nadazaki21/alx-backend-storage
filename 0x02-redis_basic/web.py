933c617355bb44813c6aae21511f1d54e553fdac
nadazkai21 <nadazaki019@gmail.com>
2024-08-15 06:32:55 +0300

    100

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,14 +2,28 @@
 """ Advanced Task """
 import requests
 import redis
+from typing import Callable
+from functools import wraps
 
 redis_obj = redis.Redis()
 count = 0
+
+
+def wrapper_function(f: Callable) -> Callable:
+    """wrapper function"""
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        count = count + 1
+        redis_obj.setex(f"count:{args}", 10, count)
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+@wrapper_function
 def get_page(url: str) -> str:
-    """ uses the requests module to obtain the
-    HTML content of a particular URL and returns it. """
+    """uses the requests module to obtain the
+    HTML content of a particular URL and returns it."""
     data = requests.get(url)
-    count = count + 1
-    redis_obj.setex(f"count:{url}",10, count)
-    
     return data.text
