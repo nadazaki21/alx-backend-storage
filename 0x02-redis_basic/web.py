5db068fd75baf9d273a8dfdf3ee4a94eeac936ad
nadazkai21 <nadazaki019@gmail.com>
2024-08-15 07:03:19 +0300

    100

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-""" Advanced Task """
-import requests
+""" expiring web cache module """
+
 import redis
+import requests
 from typing import Callable
 from functools import wraps
 
-redis_obj = redis.Redis()
+redis = redis.Redis()
 
 
-def wrapper_function(fn: Callable) -> Callable:
-    """wrapper function"""
+def wrap_requests(fn: Callable) -> Callable:
+    """Decorator wrapper"""
 
     @wraps(fn)
-    def wrapper(url: str):
-        redis_obj.incr(f"count:{url}")
-        # the cached data may still have not expired
-        cached_data = redis_obj.get(f"cached:{url}")
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        response = fn(url)
-        redis_obj.setex(f"cached:{url}", 10, response)
-        return response
+    def wrapper(url):
+        """Wrapper for decorator guy"""
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode("utf-8")
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
 
     return wrapper
 
 
-@wrapper_function
+@wrap_requests
 def get_page(url: str) -> str:
-    """uses the requests module to obtain the
-    HTML content of a particular URL and returns it."""
-    data = requests.get(url)
-    return data.text
+    """get page self descriptive"""
+    response = requests.get(url)
+    return response.text
