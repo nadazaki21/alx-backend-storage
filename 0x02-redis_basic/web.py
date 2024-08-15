c77c8ba0023f4d39de96a8486ba92c8b869e4ef6
nadazkai21 <nadazaki019@gmail.com>
2024-08-15 07:00:04 +0300

    100

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,17 +6,22 @@ from typing import Callable
 from functools import wraps
 
 redis_obj = redis.Redis()
-count = 0
 
 
 def wrapper_function(f: Callable) -> Callable:
     """wrapper function"""
 
     @wraps(f)
-    def wrapper(*args, **kwargs):
-        count = count + 1
-        redis_obj.setex(f"count:{args}", 10, count)
-        return f(*args, **kwargs)
+    def wrapper(url: str, *args, **kwargs):
+        redis_obj.incr(f"count:{url}")
+        # the cached data may still have not expired
+        cached_data = redis_obj.get(f"cached:{url}")
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        response = f(url, *args, **kwargs)
+        redis_obj.setex(f"cached:{url}", 10, response)
+        return response
 
     return wrapper
 
